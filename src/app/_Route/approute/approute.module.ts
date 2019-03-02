49c37de2ecae92ec9b1c5fe6f488ec3fcb9f40b4
Neha Verma <Neha.Verma@in.niit.com>
2019-03-02 18:33:19 +0530

    approute module add path

--- src/app/_Route/approute/approute.module.ts
+++ src/app/_Route/approute/approute.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+import { CourcesComponent } from 'src/app/cources/cources.component';
+import { FormComponent } from 'src/app/_Form/form/form.component';
 
 
 
+const appRoute: Routes=[
+  {
+    path: 'home',
+    component: AppComponent
+  },
+  {
+    path:'cources',
+    component: CourcesComponent
+  },
+  {
+    path:'form',
+    component:FormComponent
+  },
+  {
+    path:'',
+    component:AppComponent
+  },
+  {
+    path:'**',
+    component:PagenotfoundComponent
+  }
+
+
+]
 
 @NgModule({
   imports: [    
